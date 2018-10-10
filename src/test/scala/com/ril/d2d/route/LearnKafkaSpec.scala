75a2349eec036f8c791249231947b15abfda5ccf
Aniruddha Ujlamkar <aujlambkar@equalexperts.com>
2018-10-10 12:30:42 +0530

    Test added for Consuming Message from kafka

--- src/test/scala/com/ril/d2d/route/LearnKafkaSpec.scala
+++ src/test/scala/com/ril/d2d/route/LearnKafkaSpec.scala
@@ -1,37 +1,54 @@
 package com.ril.d2d.route
 
+import java.time.Duration
 import java.util.Properties
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
-import org.scalatest.{FlatSpec, Matchers}
-import org.scalatest._
+import org.apache.kafka.common.TopicPartition
+import org.scalatest.{Matchers, _}
+
+import scala.collection.JavaConversions._
 
 
 class LearnKafkaSpec extends WordSpec with Matchers {
 
   "Kafka" should {
-    val brokers = "100.96.8.107:9092"
+    val brokers = "100.96.4.108:9092"
+    val topic = "test"
 
     "produce message" in {
 
-      produceMessage(brokers, "test")
+      produceMessage(brokers, topic)
 
-      1 should be (1)
+      1 should be(1)
     }
 
-    "consume messages" in {
-      val props = new Properties()
-      props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
-      props.put(ConsumerConfig.GROUP_ID_CONFIG, "1")
-      props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
-      props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
-      props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000")
-      props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-      props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    "consume message" in {
+      val props = createConsumerConfig(brokers)
+      val consumer = new KafkaConsumer[String, String](props)
+
+      val partition = new TopicPartition(topic, 0)
+
+      consumer.assign(Seq(partition))
+
+      val offsetMetadata = consumer.committed(partition)
+      val offset = offsetMetadata.offset()
+
+      consumer.seek(partition, offset)
+
+      produceMessage(brokers, topic)
+      Thread.sleep(500)
+
+      val consumerRecordsPerPartition = consumer.poll(Duration.ofMillis(1000L))
+      consumer.commitSync()
+
+      val firstRecord = consumerRecordsPerPartition.records(partition).head
+
+      firstRecord.key() should be("any key")
+      firstRecord.value() should be("Test message 123")
     }
   }
-
 
 
   def produceMessage(brokers: String, topic: String) {
@@ -53,8 +70,16 @@ class LearnKafkaSpec extends WordSpec with Matchers {
     producer.close()
   }
 
-  def startConsumer(): Unit = {
-
+  def createConsumerConfig(brokers: String): Properties = {
+    val props = new Properties()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "1")
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
+    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "500")
+    props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props
   }
 
 }
